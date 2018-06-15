276a43b7219979b04af1978e2a5eeef82f7be7ff
Ifeanyichukwu Onyeabor <aicee@aicee.net>
2018-06-15 18:40:52 +0100

    TODO in DetailActivity.java
    // DONE (2) Display the weather forecast that was passed from MainActivity
            Intent intent = getIntent();
            detailsTextView = (TextView) findViewById(R.id.weather_details_textview);
            if (intent != null) {
                if (intent.hasExtra(Intent.EXTRA_TEXT)) {
                    weatherForDay = intent.getStringExtra(Intent.EXTRA_TEXT);
                    detailsTextView.setText(weatherForDay);
                }
            }

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,9 +1,13 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+    TextView detailsTextView;
+    String weatherForDay;
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
 
@@ -12,6 +16,14 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        // DONE (2) Display the weather forecast that was passed from MainActivity
+        Intent intent = getIntent();
+        detailsTextView = (TextView) findViewById(R.id.weather_details_textview);
+        if (intent != null) {
+            if (intent.hasExtra(Intent.EXTRA_TEXT)) {
+                weatherForDay = intent.getStringExtra(Intent.EXTRA_TEXT);
+                detailsTextView.setText(weatherForDay);
+            }
+        }
     }
 }
